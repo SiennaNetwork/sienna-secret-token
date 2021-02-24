ac099e686dac7290dd232c7369f0eeac22457854
Asparuh Kamenov <asparuhkamenov@gmail.com>
2021-02-24 11:33:13 +0200

    Implement init

--- dex/src/msg.rs
+++ dex/src/msg.rs
@@ -1,27 +1,25 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use cosmwasm_std::{HumanAddr};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
-    pub count: i32,
+    pub token_addr: HumanAddr,
+    pub symbol: String,
+    pub name: String,
+    pub decimals: u8
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    Increment {},
-    Reset { count: i32 },
+    AddLiquidity,
+    RemoveLiquidity
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
-}
-
-// We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+    AddLiquidity,
+    RemoveLiquidity
 }
--- dex/src/state.rs
+++ dex/src/state.rs
@@ -1,21 +1,62 @@
+use std::any::type_name;
 use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
-use cosmwasm_std::{CanonicalAddr, Storage};
-use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use secret_toolkit::serialization::{Bincode2, Serde};
+use cosmwasm_std::{HumanAddr, ReadonlyStorage, StdError, StdResult, Storage};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: CanonicalAddr,
+#[derive(Serialize, Deserialize, JsonSchema)]
+pub struct Config {
+    pub token_addr: HumanAddr,
+    pub factory_addr: HumanAddr,
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8
 }
 
-pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
-    singleton(storage, CONFIG_KEY)
+/// Returns StdResult<()> resulting from saving the config to storage
+///
+/// # Arguments
+///
+/// * `storage` - a mutable reference to the storage this item should go to
+/// * `config` - a reference to a Config struct
+pub fn save_config(storage: &mut impl Storage, config: &Config) -> StdResult<()> {
+    save(storage, CONFIG_KEY, config)
 }
 
-pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
-    singleton_read(storage, CONFIG_KEY)
+/// Returns StdResult<()> resulting from saving the config to storage
+///
+/// # Arguments
+///
+/// * `storage` - a reference to the storage this item should go to
+pub fn load_config(storage: &impl ReadonlyStorage) -> StdResult<Config> {
+    load(storage, CONFIG_KEY)
+}
+
+fn save<T: Serialize, S: Storage>(storage: &mut S, key: &[u8], value: &T) -> StdResult<()> {
+    storage.set(key, &Bincode2::serialize(value)?);
+    Ok(())
+}
+
+fn remove<S: Storage>(storage: &mut S, key: &[u8]) {
+    storage.remove(key);
+}
+
+fn load<T: DeserializeOwned, S: ReadonlyStorage>(storage: &S, key: &[u8]) -> StdResult<T> {
+    Bincode2::deserialize(
+        &storage
+            .get(key)
+            .ok_or_else(|| StdError::not_found(type_name::<T>()))?,
+    )
+}
+
+fn may_load<T: DeserializeOwned, S: ReadonlyStorage>(
+    storage: &S,
+    key: &[u8],
+) -> StdResult<Option<T>> {
+    match storage.get(key) {
+        Some(value) => Bincode2::deserialize(&value).map(Some),
+        None => Ok(None),
+    }
 }
